37fa5e96c8efb8a6ed68c00f067b5fd42e0c68ce
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2020-07-09 04:59:33 +0430

    first commit

---
+++ src/main/java/com/example/jaksonexample/JaksonExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaksonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaksonExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaksonExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jaksonexample/JaksonExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaksonexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaksonExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
